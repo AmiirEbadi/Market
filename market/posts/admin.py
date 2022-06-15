aedf5144ba02debab5c5fa493972cacc847c4ce0
amir <amir.d.2116@gmail.com>
2022-06-15 20:04:41 +0430

    2nd commit some stuff added in Admin Pgae

--- market/posts/admin.py
+++ market/posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from market.posts.models import Category, Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', )
+    list_filter = ('created_at', )
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', )
